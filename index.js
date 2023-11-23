cbb87a6108518da600743a6cb911a29260211f31
ashwinilakshmanan <ashwinilakshmanan1@gmail.com>
2023-11-23 11:09:50 +0530

    created login and register routes

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import AuthRoute from './Routes/AuthRoute.js'
+import UserRoute from './Routes/UserRoute.js'
+
+//routes
 
 const app = express();
+
+//middleware
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
@@ -13,3 +19,7 @@ mongoose
 .connect(process.env.MONGO_DB,{useNewUrlParser:true, useUnifiedTopology:true} )
     .then(() => app.listen(process.env.PORT, () => console.log(`Listening at ${process.env.PORT} `)))
     .catch((error)=>console.log(error))
+
+//usage of routes
+app.use('/auth',AuthRoute);
+app.use('/user',UserRoute)
