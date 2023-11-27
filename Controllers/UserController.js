d6f91af514aad4fcb9dff01803b8133666ce4cce
Ashwini <ashwinilakshmanan1@gmail.com>
2023-11-27 11:47:41 +0530

    created the delete functionality

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,4 +1,5 @@
 import UserModel from "../Models/UserModel.js";
+import bcrypt from 'bcrypt'
 
 //get a User
 
@@ -27,10 +28,40 @@ export const updateUser = async(req,res)=>{
 
     if(id === currentUserId || currentUserAdminStatus){
         try {
-            const user = await UserModel.findByIdAndUpdate(id,req.body,{new:true})
+
+            if(password){
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(password,salt)
+            }
+
+            const user = await UserModel.findByIdAndUpdate(id, req.body,{new:true})
+
             res.status(200).json(user)
+
         } catch (error) {
             res.status(500).json(error)
         }
+    }
+    else{
+        res.status(403).json("Access Denied")
+    }
+}
+
+//delete user
+export const deleteUser = async(req,res)=>{
+    const id = req.params.id
+
+    const {currentUserId,currentUserAdminStatus} = req.body
+
+    if(currentUserId ===id || currentUserAdminStatus){
+        try {
+            await UserModel.findByIdAndDelete(id);
+            res.status(200).json("User deleted successfully")
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+    else{
+        res.status(403).json("Access Denied! You can only delete your own profile")
     }
 }
