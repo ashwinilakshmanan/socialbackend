3c431edf95fd05f583e61faeb1bf767268bd9d33
Ashwini <ashwinilakshmanan1@gmail.com>
2023-11-27 10:48:00 +0530

    created get user by its id

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,9 +1,8 @@
 import express from "express"
+import { getUser } from "../Controllers/UserController.js";
 
 const router = express.Router()
 
-router.get('/', async(req,res)=>{
-    res.send("user route")
-})
+router.get('/:id', getUser)
 
 export default router;
