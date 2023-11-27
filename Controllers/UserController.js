327c28f5ed72dddad2190fa0fc9d46b2a19b5289
Ashwini <ashwinilakshmanan1@gmail.com>
2023-11-27 11:05:32 +0530

    created update user

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -19,3 +19,18 @@ export const getUser =  async(req,res)=>{
         
     }
 }
+
+//update user
+export const updateUser = async(req,res)=>{
+    const id =req.params.id;
+    const {currentUserId,currentUserAdminStatus,password} = req.body
+
+    if(id === currentUserId || currentUserAdminStatus){
+        try {
+            const user = await UserModel.findByIdAndUpdate(id,req.body,{new:true})
+            res.status(200).json(user)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+}
