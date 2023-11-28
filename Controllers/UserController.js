71c1ecee36b1dfb60b3a340dd2b77a3779f4e7af
ashwinilakshmanan <ashwinilakshmanan1@gmail.com>
2023-11-28 11:38:02 +0530

    created the api for unfollow

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -3,38 +3,38 @@ import bcrypt from 'bcrypt'
 
 //get a User
 
-export const getUser =  async(req,res)=>{
+export const getUser = async (req, res) => {
     const id = req.params.id;
 
     try {
         const user = await UserModel.findById(id)
-        if(user){
-            const {password, ...otherDetails} = user._doc
+        if (user) {
+            const { password, ...otherDetails } = user._doc
             res.status(200).json(otherDetails)
         }
-        else{
+        else {
             res.status(404).json("No such user exists")
         }
     } catch (error) {
         res.status(500).json(error)
-        
+
     }
 }
 
 //update user
-export const updateUser = async(req,res)=>{
-    const id =req.params.id;
-    const {currentUserId,currentUserAdminStatus,password} = req.body
+export const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const { currentUserId, currentUserAdminStatus, password } = req.body
 
-    if(id === currentUserId || currentUserAdminStatus){
+    if (id === currentUserId || currentUserAdminStatus) {
         try {
 
-            if(password){
+            if (password) {
                 const salt = await bcrypt.genSalt(10);
-                req.body.password = await bcrypt.hash(password,salt)
+                req.body.password = await bcrypt.hash(password, salt)
             }
 
-            const user = await UserModel.findByIdAndUpdate(id, req.body,{new:true})
+            const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true })
 
             res.status(200).json(user)
 
@@ -42,18 +42,18 @@ export const updateUser = async(req,res)=>{
             res.status(500).json(error)
         }
     }
-    else{
+    else {
         res.status(403).json("Access Denied")
     }
 }
 
 //delete user
-export const deleteUser = async(req,res)=>{
+export const deleteUser = async (req, res) => {
     const id = req.params.id
 
-    const {currentUserId,currentUserAdminStatus} = req.body
+    const { currentUserId, currentUserAdminStatus } = req.body
 
-    if(currentUserId ===id || currentUserAdminStatus){
+    if (currentUserId === id || currentUserAdminStatus) {
         try {
             await UserModel.findByIdAndDelete(id);
             res.status(200).json("User deleted successfully")
@@ -61,32 +61,60 @@ export const deleteUser = async(req,res)=>{
             res.status(500).json(error)
         }
     }
-    else{
+    else {
         res.status(403).json("Access Denied! You can only delete your own profile")
     }
 }
 
 //Follow a User
-export const followUser = async(req,res)=>{
-    const id= req.params.id
+export const followUser = async (req, res) => {
+    const id = req.params.id
 
-    const {currentUserId} = req.body
+    const { currentUserId } = req.body
 
-    if(currentUserId === id){
+    if (currentUserId === id) {
         res.status(403).json("Forbidden Error")
     }
-    else{
+    else {
         try {
             const followUser = await UserModel.findById(id);
             const followingUser = await UserModel.findById(currentUserId)
 
-            if(!followUser.followers.includes(currentUserId)){
-                await followUser.updateOne({$push : {followers:currentUserId}})
-                await followingUser.updateOne({$push : {following: id}})
+            if (!followUser.followers.includes(currentUserId)) {
+                await followUser.updateOne({ $push: { followers: currentUserId } })
+                await followingUser.updateOne({ $push: { following: id } })
                 res.status(200).json("User followed!")
             }
-            else{
+            else {
                 res.status(403).json("User is Already followed by You")
+            }
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+}
+
+//UnFollow a User
+export const unFollowUser = async (req, res) => {
+    const id = req.params.id
+
+    const { currentUserId } = req.body
+
+    if (currentUserId === id) {
+        res.status(403).json("Forbidden Error")
+    }
+    else {
+        try {
+            const followUser = await UserModel.findById(id);
+            const followingUser = await UserModel.findById(currentUserId)
+
+            if (followUser.followers.includes(currentUserId)) {
+                await followUser.updateOne({ $pull: { followers: currentUserId } })
+                await followingUser.updateOne({ $pull: { following: id } })
+                res.status(200).json("User UnFollowed!")
+            }
+            else {
+                res.status(403).json("User is not followed by You")
             }
         } catch (error) {
             res.status(500).json(error)
